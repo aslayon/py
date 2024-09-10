34ee704db1d489b386a47e73502d6a17f3beefeb
Minseok Chu <mchu7797@gmail.com>
2024-09-10 15:15:40 +0900

    Update cnn_torch.py
    시그모이드 변경

--- CNN_torch/cnn_torch.py
+++ CNN_torch/cnn_torch.py
@@ -28,14 +28,14 @@ class CNN(nn.Module):
         self.fc2 = nn.Linear(in_features=128, out_features=64)
         self.fc3 = nn.Linear(in_features=64, out_features=10) 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
         x = x.view(-1, 64*7*7)  # Flatten
-        x = self.relu(self.fc1(x))  # 은닉층 1
-        x = self.relu(self.fc2(x))  # 은닉층 2
+        x = self.sigmoid(self.fc1(x))  # 은닉층 1
+        x = self.sigmoid(self.fc2(x))  # 은닉층 2
         x = self.fc3(x)  # 출력층
         return x
 
